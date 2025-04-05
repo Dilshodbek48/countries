dbffaa7592cf5620a21f031f0afe7e40c5799f4f
Dilshodbek48 <xolmominovdilshodbek8888@gmail.com>
2025-04-06 00:23:36 +0500

    add toggler button locatstorage.setitem

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 const elSiteThemeToggler = document.querySelector('.js-site-header-theme-toggler');
 
-elSiteThemeToggler.addEventListener('click', function () {
-  document.body.classList.toggle('dark-mode');
-});
+
+if (localStorage.getItem('name') === 'dark-mode') {
+  document.body.classList.add('dark-mode');
+}
+
+if (elSiteThemeToggler) {
+  elSiteThemeToggler.addEventListener('click', function() {
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('name', 'light-mode');
+      document.body.classList.remove('dark-mode');
+    } else {
+      localStorage.setItem('name', 'dark-mode');
+      document.body.classList.add('dark-mode');
+    }
+  });
+}
